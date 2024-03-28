f786b812a1fb15afab6e03e379bef7f482c43e75
ahmtkla <esogu221120221012@gmail.com>
2024-03-28 10:08:39 +0300

    12. video yapıldı tarif önerme sayfası sql ile ilişkilendirildi

--- SiteYemek/TarifOner.aspx.cs
+++ SiteYemek/TarifOner.aspx.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace SiteYemek
 {
     public partial class TarifOner : System.Web.UI.Page
     {
+        sqlsinif bgl = new sqlsinif();
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +18,16 @@ namespace SiteYemek
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            SqlCommand komut = new SqlCommand("insert into Tbl_Tarifler(TarifAd,TarifMalzeme,TarifYapılıs,TarifResim,TarifSahip,TarifSahipMail) values (@t1,@t2,@t3,@t4,@t5,@t6)",bgl.baglanti());
+            komut.Parameters.AddWithValue("@t1",TxtTarifAd.Text);
+            komut.Parameters.AddWithValue("@t2", TxtMalzemeler.Text);
+            komut.Parameters.AddWithValue("@t3", TxtYapilis.Text);
+            komut.Parameters.AddWithValue("@t4", FileUplo.FileName);
+            komut.Parameters.AddWithValue("@t5", TxtTarifOneren.Text);
+            komut.Parameters.AddWithValue("@t6", TxtMailAdresi.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            Response.Write("Tarifiniz Alınmıştır");
         }
     }
 }
